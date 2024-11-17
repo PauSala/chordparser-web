008307e3ac8a6b532f8f274a52ad5811f2074860
pausala <pau.salaf@gmail.com>
2024-11-17 09:17:20 +0100

    sounds

--- www/app/utils/playChord.ts
+++ www/app/utils/playChord.ts
@@ -4,39 +4,42 @@ const midiToFrequency = (midi: number) => {
     return Tone.Frequency(midi, "midi").toFrequency();
 };
 
-const synth = new Tone.PolySynth(Tone.AMSynth, {
-    oscillator: {
-        type: 'fatsine3'
-    },
-    envelope: {
-        attack: 0.01,   
-        decay: 0.3,      
-        sustain: 0.2,    
-        release: 1.1    
-    }
-});
 
+export const playChord = async (midiCodes: number[]) => {
 
-const synth2 = new Tone.PolySynth(Tone.Synth, {
-    oscillator: {
-        type: 'fattriangle20'
-    },
-    envelope: {
-        attack: 0.01,   
-        decay: 0.3,      
-        sustain: 0.2,    
-        release: 1.1    
-    }
-});
+    await Tone.start();
 
-const gain = new Tone.Gain(0.3).toDestination(); // Lowered the gain for a softer sound
-
-const reverb = new Tone.Reverb({
-    decay: 1.9,         
-    wet: 0.8            
-}).toDestination();
-
-export const playChord = (midiCodes: number[]) => {
+    const synth = new Tone.PolySynth(Tone.AMSynth, {
+        oscillator: {
+            type: 'fatsine3'
+        },
+        envelope: {
+            attack: 0.01,   
+            decay: 0.3,      
+            sustain: 0.2,    
+            release: 1.1    
+        }
+    });
+    
+    
+    const synth2 = new Tone.PolySynth(Tone.Synth, {
+        oscillator: {
+            type: 'fattriangle3'
+        },
+        envelope: {
+            attack: 0.01,   
+            decay: 0.3,      
+            sustain: 0.2,    
+            release: 1.1    
+        }
+    });
+    
+    const gain = new Tone.Gain(0.3).toDestination(); // Lowered the gain for a softer sound
+    
+    const reverb = new Tone.Reverb({
+        decay: 1.9,         
+        wet: 0.8            
+    }).toDestination();
 
 
     synth.connect(gain).connect(reverb); 
